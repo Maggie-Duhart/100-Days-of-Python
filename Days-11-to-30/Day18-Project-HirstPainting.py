f10b30277e47fe839d314bec241bb830fd696b8b
Maggie <104781899+Maggie-Duhart@users.noreply.github.com>
2024-01-08 15:58:24 -0500

    Update and rename main.py to Day18-Project-HirstPainting.py

--- Days-11-to-30/Day18-Project-HirstPainting.py
+++ Days-11-to-30/Day18-Project-HirstPainting.py
@@ -1,17 +1,3 @@
-# import colorgram
-#
-# rgb_colors = []
-# # Extract 30 colors from an image
-# colors = colorgram.extract('image.jpg', 30)
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b)
-#     rgb_colors.append(new_color)
-#
-# print(rgb_colors)
-
 from turtle import Screen
 import turtle as t
 import random
